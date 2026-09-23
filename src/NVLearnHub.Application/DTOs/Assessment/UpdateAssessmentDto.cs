89c6083e3beecd05c4fcb2cbf21fbe847d264ad4
NEWVISIONSOFTPU\komal.godase <komal.godase@newvision-software.com>
2026-09-23 19:12:16 +0530

    Admin apis changes

---
+++ src/NVLearnHub.Application/DTOs/Assessment/UpdateAssessmentDto.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NVLearnHub.Application.DTOs.Assessment
+{
+    public class UpdateAssessmentDto
+    {
+        public int CourseId { get; set; }
+
+        public string Title { get; set; } = default!;
+
+        public int TimeLimitMinutes { get; set; }
+
+        public int PassPercentage { get; set; }
+
+        public int MaxAttempts { get; set; }
+
+        public List<UpdateQuestionDto> Questions { get; set; } = new();
+    }
+
+    public class UpdateQuestionDto
+    {
+        public string QuestionText { get; set; } = default!;
+
+        public int OrderIndex { get; set; }
+
+        public List<UpdateQuestionOptionDto> Options { get; set; } = new();
+    }
+
+    public class UpdateQuestionOptionDto
+    {
+        public string OptionText { get; set; } = default!;
+
+        public bool IsCorrect { get; set; }
+    }
+}
